453f70606a16a235030b75caba21433077cdadf3
dhc518 <dhc000518@gmail.com>
2022-10-30 22:04:37 +0900

    add folder & move to it

--- lesson/grass.py
+++
@@ -1,8 +0,0 @@
-from pico2d import *
-
-class Grass:
-    def __init__(self):
-        self.image = load_image('grass.png')
-
-    def draw(self):
-        self.image.draw(400, 30)
--- lesson/mygame.py
+++
@@ -1,8 +0,0 @@
-import game_framework
-import pico2d
-
-import play_state
-
-pico2d.open_canvas()
-game_framework.run(play_state)
-pico2d.close_canvas()
